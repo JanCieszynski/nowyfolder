74dc71f96073757a54823d282a5f8e1421e5ccfc
Jan Cieszynski <janekcieszynski1@gmail.com>
2023-03-30 09:17:13 +0200

    tRa

---
+++ scenes/initial.js
@@ -0,0 +1,8 @@
+const generateLeftBasic = () =>{
+   let leftContent =  `<div id="lewy">
+   <div class="dialogelse" onclick="oknaDialogowe()" data-options="Wsiądź,Gdzie ja jestem?">
+       Co tak stoisz klapsie?
+     </div>
+</div>`
+return leftContent;
+}
